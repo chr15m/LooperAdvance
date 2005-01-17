c7d1e0ebb376ea473b8f0d00a916bf725c23b604
chrism <chrism@eec3997f-6950-4fa9-abeb-60fabf57e09c>
2005-01-17 05:29:18 +0000

    Makefile: changed samples to be signed
    buildsamplebank.py: multiple samples to one file with a struct SampleData which stores data, length, and name
    
    
    git-svn-id: file:///home/chrism/dev/old-cvs/old-cvs/trunk/looper@100 eec3997f-6950-4fa9-abeb-60fabf57e09c

--- samples/buildsamplebank.py
+++ samples/buildsamplebank.py
@@ -33,12 +33,24 @@ if (len(sys.argv) > 1):
 	# the names of all our samples
 	samples = []
 	
+	# create the header bits
+	htext.append("// This file is produced automatically by " + sys.argv[0])
+	htext.append("")
+	htext.append("#define NUMSAMPLES " + str(numsamples));
+	htext.append('#include "typedefs.h"')
+	htext.append('#include "SampleData.hh"')
+	htext.append("")
+	
+	ctext.append("// This file is produced automatically by " + sys.argv[0])
+	ctext.append("")
+	ctext.append('#include"samples.hh"')
+	ctext.append("")
+	
 	# for every sample file
 	for filename in sys.argv[1:]:
 		print "processing " + filename
 		
 		shortname = filename[:-4]
-		samples.append(shortname)
 		
 		# try and open the file
 		binfile = 0
@@ -47,17 +59,7 @@ if (len(sys.argv) > 1):
 		except:
 			print "Couldn't open " + filename
 		
-		htext.append("// This file is produced automatically by " + sys.argv[0])
-		htext.append("")
-		htext.append("#define NUMSAMPLES " + str(numsamples));
-		htext.append('#include "typedefs.h"')
-		htext.append("")
-		
-		ctext.append("// This file is produced automatically by " + sys.argv[0])
-		ctext.append("")
-		ctext.append('#include"samples.hh"')
-		ctext.append("")
-
+		# do we have a real file
 		if (binfile):	
 			filesize = 0
 			
@@ -69,25 +71,34 @@ if (len(sys.argv) > 1):
 				byte = binfile.read(1)
 				if (byte):
 					# add this byte to our data array
-					adata.append(str(hex(ord(byte))))
+					adata.append(ord(byte))
 					filesize = filesize + 1
 			binfile.close()
 			
-			htext.append('extern const u8 ' + shortname + "[" + str(filesize) + "];")
-			ctext.append('const u8 ' + shortname + "[" + str(filesize) + "] = {")
+			# create entries in the .cc and .hh data file for this particular sample
+			htext.append('extern const s8 ' + shortname + "[" + str(filesize) + "];")
+			ctext.append('const s8 ' + shortname + "[" + str(filesize) + "] = {")
 			
+			# now loop through all of our data writing it to the .cc file
+			datachunks = []
 			for seek in range(len(adata)):
-				ctext.append(adata[seek] + ", ")
-				if ((seek + 1) % 8 == 0):
-					ctext.append("")
+				datachunks.append("0x%02x" % adata[seek])
+				# put a carriage return to make the file neater
+				if ((seek + 1) % 32 == 0):
+					ctext.append(string.join(datachunks, ", ") + ", ")
+					datachunks = []
 			
 			ctext.append("};\n")
+		
+		samples.append([shortname, filesize])
+
+	# write an entry in the samples data table for every sample we've added
+	ctext.append("const SampleData samples[NUMSAMPLES] = {")
+	for bits in samples:
+		ctext.append("{(void *)" + bits[0] + ", " + str(bits[1]) + ', "' + bits[0] + '"},')
+	ctext.append("};")
 	
-	ctext.append("const u8* samples[NUMSAMPLES] = {" + string.join(samples, ", ") + "};")
-	ctext.append("const char* samplenames[NUMSAMPLES] = {" + string.join(map(lambda x: '"' + x + '"', samples), ", ") + "};")
-	
-	htext.append("extern const u8 *samples[NUMSAMPLES];")
-	htext.append("extern const char *samplenames[NUMSAMPLES];")
+	htext.append("extern const SampleData samples[NUMSAMPLES];")
 
 	# open our c and h files for writing
 	cfile=open("samples.cc", "w")
